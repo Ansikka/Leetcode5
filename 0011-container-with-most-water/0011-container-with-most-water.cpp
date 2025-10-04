1a6441ce60007428b80f7a7b636c9cd758fe9cd1
Anshika Sharma <succes20s23@gmail.com>
2025-10-04 17:27:45 +0530

    Time: 1 ms (46.22%), Space: 62.8 MB (78.7%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -7,17 +7,13 @@ public:
       
         // Use two-pointer technique to find maximum water container
         while (left < right) {
-            // Calculate current water area: minimum height × width
-            // Water level is limited by the shorter line
+           
             int currentArea = min(height[left], height[right]) * (right - left);
           
             // Update maximum water area if current is larger
             maxWater = max(maxWater, currentArea);
           
-            // Move the pointer pointing to the shorter line inward
-            // This greedy approach works because:
-            // - Moving the shorter line might find a taller line (potentially increasing area)
-            // - Moving the taller line will definitely decrease width without guarantee of height increase
+           
             if (height[left] < height[right]) {
                 ++left;  // Move left pointer rightward if left line is shorter
             } else {
