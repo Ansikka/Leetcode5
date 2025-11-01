ce401baf2749cb57645ce806117bdea5ca029b42
Anshika Sharma <succes20s23@gmail.com>
2025-11-01 16:50:25 +0530

    Time: 101 ms (51.85%), Space: 263.4 MB (58.72%) - LeetHub

---
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+         unordered_set<int> numsSet(nums.begin(), nums.end());
+      
+       
+        ListNode* dummyNode = new ListNode(0, head);
+      
+        // Iterate through the linked list using a previous pointer
+        for (ListNode* prevNode = dummyNode; prevNode->next != nullptr;) {
+            // Check if the next node's value exists in the set
+            if (numsSet.count(prevNode->next->val)) {
+                // Remove the next node by updating the pointer
+                prevNode->next = prevNode->next->next;
+                // Note: prevNode stays at the same position for the next iteration
+            } else {
+                // Move to the next node only if no deletion occurred
+                prevNode = prevNode->next;
+            }
+        }
+      
+        // Return the modified list (head might have changed)
+        return dummyNode->next;
+    }
+};
