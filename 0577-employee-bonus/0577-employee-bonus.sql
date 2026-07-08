3daa117475f289f53d1f7ef8c9bd8dccd26d9df2
Anshika Sharma <succes20s23@gmail.com>
2026-07-08 16:18:19 +0530

    Time: 1138 ms (25.47%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
-SELECT name, bonus
-FROM
-    Employee
-    LEFT JOIN Bonus USING (empId)
-WHERE IFNULL(bonus, 0) < 1000;
+
+SELECT Employee.name, Bonus.bonus
+FROM Employee
+LEFT JOIN Bonus
+  USING (empId)
+WHERE IFNULL(Bonus.bonus, 0) < 1000;
